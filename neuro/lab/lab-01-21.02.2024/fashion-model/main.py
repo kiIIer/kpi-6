fd655bf0faf8df1dcdda7d5f6aaace9425e1007f
kiIIer <39669501+kiIIer@users.noreply.github.com>
2024-02-21 05:56:08 +0200

    Neuro/lab/1 (#15)
    * code for neuro lab 1
    
    * neuro lab 1 report

---
+++ neuro/lab/lab-01-21.02.2024/fashion-model/main.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(256, activation='relu'),
+    Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=20)
+
+test_loss, test_accuracy = model.evaluate(test_images, test_labels, verbose=2)
+
+print(f'\nTest accuracy: {test_accuracy * 100:.2f}%')
